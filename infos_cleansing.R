4c2405ddabedccaeb31a4e14bff7da7c7d0861c9
danaiav <danaia@aueb.gr>
2016-09-24 00:29:57 +0300

    Delete not necessary files

--- infos_cleansing.R
+++
@@ -1,5 +0,0 @@
-loading_times <- read.csv("F:/Dropbox/Dani/Spinellis - Diplwmatiki/Python Scripts/infos/loading_times.csv", sep=";")
-loading_times$loading.time <- format(loading_times$loading.time, decimal.mark=",")
-str(loading_times)
-loading_times$X <- NULL
-write.csv2(loading_times, file = "F:/Dropbox/Dani/Spinellis - Diplwmatiki/Python Scripts/infos/loading_time.csv", sep=";")
